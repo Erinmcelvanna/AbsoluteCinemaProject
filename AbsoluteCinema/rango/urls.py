0e0ed7603e4b6d40ebe175660ad2083bb7cd0826
Xufei Cao <3075145C@student.gla.ac.uk>
2026-03-24 23:06:21 +0000

    update urls

--- AbsoluteCinema/rango/urls.py
+++ AbsoluteCinema/rango/urls.py
@@ -1,7 +1,23 @@
 from django.urls import path
 from . import views
 
-
 urlpatterns = [
     path('', views.index, name='index'),
+    path('home/', views.home, name='home'),
+
+    path('movies/', views.movies, name='movies'),
+    path('movies/<int:movie_id>/', views.movie_detail, name='movie_detail'),
+
+    path('movies/<int:movie_id>/rate/', views.rate_movie, name='rate_movie'),
+    path('movies/<int:movie_id>/reviews/', views.movie_reviews, name='movie_reviews'),
+
+    path('ranked/', views.ranked, name='ranked'),
+
+    path('profile/', views.profile, name='profile'),
+    path('profile/favourites/', views.favourites, name='favourites'),
+    path('profile/add_favourite/<int:movie_id>/', views.add_favourite, name='add_favourite'),
+    path('profile/remove_favourite/<int:movie_id>/', views.remove_favourite, name='remove_favourite'),
+
+    path('profile/watch-history/', views.watch_history, name='watch_history'),
+    path('profile/add_watch/<int:movie_id>/', views.add_watch_history, name='add_watch_history'),
 ]
