2890c9c1e23c8952c7b74c7d72c8f14d1a710de9
Xufei Cao <3075145C@student.gla.ac.uk>
2026-03-25 01:00:06 +0000

    update urls

--- rango/urls.py
+++ rango/urls.py
@@ -5,7 +5,7 @@ app_name = 'rango'
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('home/', views.index, name='home'),
+    path('home/', views.home, name='home'),
     path('login/', views.login_view, name='login'),
     path('discover/', views.discover, name='discover'),
     path('register/', views.register_view, name='register'),
